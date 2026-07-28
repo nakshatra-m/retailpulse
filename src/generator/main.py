bd09254f80dc249fdc7a83f5f15bcc46c0eca477
nakshatra-m <nakshatra152001@gmail.com>
2026-07-28 15:55:12 -0400

    chore: remove generated datasets from version control

---
+++ src/generator/main.py
@@ -0,0 +1,33 @@
+from store_generator import save_stores
+from product_generator import save_products
+from customer_generator import save_customers
+from promotion_generator import save_promotions
+from order_generator import generate_orders
+from inventory_generator import generate_inventory
+from returns_generator import generate_returns
+
+
+
+def main():
+
+    save_stores()
+
+    save_products()
+
+    save_customers()
+
+    save_promotions()
+
+    generate_orders()
+
+    generate_inventory()
+
+    generate_returns()
+
+
+    print("\nRetailPulse data generation completed!")
+
+
+
+if __name__=="__main__":
+    main()
